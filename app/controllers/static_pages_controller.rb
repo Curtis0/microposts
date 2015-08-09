9e50fb87ca1441aa182ca32488f30728d97bbb2c
seiya21 <seiyathesaint@gmail.com>
2015-08-09 09:18:43 +0000

    corrected static_pages controller error.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build # これは、Micropost.new(user_id: current_user.id)と同じです。前者の方はcurrent_userのhas_many :micropostsで生成されるbuildメソッドを使用していて、確実にuser_idが紐付いたデータを作成できるのでこちらを使用するようにしましょう。
-    @feed_items = current_user.feed_items.includes(:user).page(params[:page]).per(5).order(created_at: :desc)
+    if logged_in?
+      @micropost = current_user.microposts.build # これは、Micropost.new(user_id: current_user.id)と同じです。前者の方はcurrent_userのhas_many :micropostsで生成されるbuildメソッドを使用していて、確実にuser_idが紐付いたデータを作成できるのでこちらを使用するようにしましょう。
+      @feed_items = current_user.feed_items.includes(:user).page(params[:page]).per(5).order(created_at: :desc)
+    end
   end
 end
